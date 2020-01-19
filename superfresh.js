e11f1aa8adec86fb43c6547257e4da6ae81d2186
wolfram77 <wolfram77@gmail.com>
2020-01-19 21:56:10 +0530

    splice()

--- superfresh.js
+++ superfresh.js
@@ -415,6 +415,23 @@ function unshift(x, ...vs) {
   return concat$(vs, x);
 }
 
+/**
+ * Removes or replaces existing values.
+ * @param {Array} x source
+ * @param {number} i remove index
+ * @param {number?} n no. of values to remove (def: all till end)
+ * @param {...any} vs values to insert
+ * @returns {Array} [0->i, vs, i+n->end]
+ */
+function splice(x, i, n=x.length-i, ...vs) {
+  var a = x.slice(0, i);
+  for(var v of vs)
+    a.push(v);
+  for(var i=i+n, I=x.length; i<I; i++)
+    a.push(x[i]);
+  return a;
+}
+
 
 
 
@@ -440,18 +457,6 @@ function skipr(x, fn, ths=null) {
   for(var i=x.length-1; i>=0; i--)
     if(fn.call(ths, x[i], i, x)!==0) break;
   return i+1;
-}
-
-
-
-
-function splice(x, i, n=1, ...vs) {
-  var a = x.slice(0, i);
-  for(var v of vs)
-    a.push(v);
-  for(var i=i+n, I=x.length; i<I; i++)
-    a.push(x[i]);
-  return a;
 }
 
 
