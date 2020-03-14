cfc078e06941afa260fd0e82b59b3393d5d12585
wolfram77 <wolfram77@gmail.com>
2020-03-14 10:28:38 +0530

    save

--- src/copyWithin.js
+++ src/copyWithin.js
@@ -4,11 +4,11 @@
  * @param {number} j write index
  * @param {number?} i read start index
  * @param {number?} I read end index
- * @returns {Array} updated
+ * @returns {Array}
  */
 function copyWithin(x, j, i=0, I=x.length) {
   var a = x.slice(0, j);
-  for(var J=j+I-i; j<J; j++, i++)
+  for(var J=Math.min(j+I-i, I); j<J; j++, i++)
     a[j] = x[i];
   for(var J=x.length; j<J; j++)
     a[j] = x[j];
