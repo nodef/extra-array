060b18d6068bb6f3918ccbddab9f1ac8996e4cc8
wolfram77 <wolfram77@gmail.com>
2020-03-15 21:59:02 +0530

    scripts2 -> unused, fresh1/2.js -> unused.js

--- unused.js
+++ unused.js
@@ -1,7 +1,3 @@
-// PURE FUNCTIONS
-// - behave like math functions
-// - dont manipulate input arrays
-
 /**
  * Gets prefix of desired length.
  * @param {Array} x array
@@ -107,24 +103,56 @@ function group(x) {
   return a;
 }
 
+// ###########################
+
+function intersect(x, y, fn) {
+  fn = fn||cmp;
+  var a = [];
+  x: for(var v of x) {
+    for(var w of y)
+      if(fn(v, w)===0) { a.push(v); continue x; }
+  }
+  return a;
+}
+
+function union$(x, y, fn) {
+  fn = fn||cmp;
+  y: for(var w of y) {
+    for(var v of x)
+      if(fn(v, w)===0) continue y;
+    x.push(w);
+  }
+}
+
+function union(x, y, fn) {
+  return union$(x.slice(), y, fn);
+}
 
 
+function takel(x, fn) {
+  return x.slice();
+}
 
+function searchl(x, fn, ths=null) {
+  for(var i=0, I=x.length; i<I; i++)
+    if(fn.call(ths, x[i], i, x)===0) break;
+  return i;
+}
 
-exports.head = head;
-exports.last = last;
-exports.init = init;
-exports.tail = tail;
-exports.inits = inits;
-exports.tails = tails;
-exports.permutations = permutations;
-exports.subsequences = subsequences;
-exports.max = max;
-exports.min = min;
-exports.take = take;
-exports.drop = drop;
-exports.takeWhile = takeWhile;
-exports.dropWhile = dropWhile;
-exports.partition = partition;
-exports.zip = zip;
-exports.append = append;
+function searchr(x, fn, ths=null) {
+  for(var i=x.length-1; i>=0; i--)
+    if(fn.call(ths, x[i], i, x)===0) break;
+  return i+1;
+}
+
+function skipl(x, fn, ths=null) {
+  for(var i=0, I=x.length; i<I; i++)
+    if(fn.call(ths, x[i], x)!==0) break;
+  return i;
+}
+
+function skipr(x, fn, ths=null) {
+  for(var i=x.length-1; i>=0; i--)
+    if(fn.call(ths, x[i], i, x)!==0) break;
+  return i+1;
+}
