d513cea41bd5fa702828eb4677b97eaa50870c98
wolfram77 <wolfram77@gmail.com>
2020-03-15 17:58:46 +0530

    src: pause

--- scripts2/copy.js
+++
@@ -1,8 +0,0 @@
-function copyTo(arr, bgn=0, end=arr.length, z=[], z0=z.length, z1=z0+(end-bgn)) {
-  if(z1>z.length) z.length = z1;
-  if(arr===z) return z.copyWithin(z0, bgn, end);
-  for(var i=bgn; i<end; i++)
-    z[z0++] = arr[i];
-  return z;
-};
-module.exports = copyTo;
