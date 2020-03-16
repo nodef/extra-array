49b074259f3014f3641f0621e5d75a3cc062b31b
wolfram77 <wolfram77@gmail.com>
2020-03-16 21:38:33 +0530

    src: +isDisjoint

---
+++ src/isDisjoint.js
@@ -0,0 +1,18 @@
+const cmp = require('./_cmp');
+
+/**
+ * Checks if arrays have no value in common.
+ * @param {Array} x an array
+ * @param {Array} y another array
+ * @param {function?} fn compare function (a, b)
+ * @returns {boolean} true if disjoint
+ */
+function isDisjoint(x, y, fn) {
+  fn = fn||cmp;
+  for(var v of y) {
+    for(var u of x)
+      if(fn(u, v)===0) return false;
+  }
+  return true;
+}
+module.exports = isDisjoint;
