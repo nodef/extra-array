4f58622cad62864225ee7265543145b1d73d8156
wolfram77 <wolfram77@gmail.com>
2020-03-19 13:42:09 +0530

    src: types for getAll(), getLerp()

--- src/getAll.js
+++ src/getAll.js
@@ -3,7 +3,7 @@ const get = require('./get');
 /**
  * Gets value at indices (+ve, -ve).
  * @param {Array} x an array
- * @param {number} is indices (-ve: from right)
+ * @param {Array<number>} is indices (-ve: from right)
  * @returns {*} [...values]
  */
 function getAll(x, is) {
--- src/getLerp.js
+++ src/getLerp.js
@@ -3,7 +3,7 @@ const get = require('./get');
 /**
  * Gets value at fractional index.
  * @param {Array} x an array
- * @param {number?} f fractional index 0->1
+ * @param {number} f fractional index 0->1
  * @returns {*} value
  */
 function getLerp(x, f) {
