72db16bc4f778c63370b49ce646ca8ea1f6409e5
wolfram77 <wolfram77@gmail.com>
2020-03-19 22:21:18 +0530

    src: splice follows pop, shift

--- src/splice.js
+++ src/splice.js
@@ -4,14 +4,15 @@
  * @param {number} i remove index
  * @param {number?} n no. of values to remove
  * @param {...any} vs values to insert
- * @returns {Array} [0->i, vs, i+n->I]
+ * @returns {Array<Array>} [[...removed], [...updated]]
  */
 function splice(x, i, n=x.length-i, ...vs) {
   var a = x.slice(0, i);
+  var r = x.slice(i, i+n);
   for(var v of vs)
     a.push(v);
   for(var i=i+n, I=x.length; i<I; i++)
     a.push(x[i]);
-  return a;
+  return [r, a];
 }
 module.exports = splice;
