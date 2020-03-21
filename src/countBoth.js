f9104312a72db78eb107b75d1d8ba34d5ac400c9
wolfram77 <wolfram77@gmail.com>
2020-03-22 02:56:38 +0530

    jsperf: says no need to optimize, if is ok

--- src/countBoth.js
+++ src/countBoth.js
@@ -7,14 +7,26 @@ const cmp = require('./_cmp');
  * @param {function?} fn compare function (a, b)
  * @returns {number} occurrences
  */
-function count(x, v, fcmp=null, fmap=null, ths=null) {
-  var fcmp = fcmp||cmp, i = -1, n = 0;
-  var fmap = fmap||id;
+function count(x, v, fn=null, fm=null, ths=null) {
+  if(fm) return countOn(x, v, fm, ths);
+  var fn = fn||cmp, n = 0;
+  for(var u of x)
+    if(fn(u, v)===0) n++;
+  return n;
+}
+function countOn(x, v, fn=null, ths=null) {
+  var i = -1, n = 0;
   var v1 = fn.call(ths, v, 0, null);
   for(var u of x) {
     var u1 = fn.call(ths, u, ++i, x);
-    if(fcmp(u1, v1)===0) n++;
+    if(u1===v1) n++;
   }
   return n;
 }
 module.exports = count;
+
+var x = [1, 2, 3, -2];
+count(x, 2, (a, b) => Math.abs(a)-Math.abs(b));
+countOn(x, 2, v => Math.abs(v));
+
+count(x, 2, null, v => Math.abs(v));
