33510b4ccc2794f1f6b3a04246f093088c0e0e94
wolfram77 <wolfram77@gmail.com>
2020-03-27 00:41:49 +0530

    main: count, countOn

--- src/countOn.js
+++ src/countOn.js
@@ -5,14 +5,14 @@ const id = require('./_id');
  * @param {Iterable} x an array
  * @param {function?} fn map function (v, i, x)
  * @param {object?} ths this argument
- * @returns {Map<any, number>}
+ * @returns {Map<any, number>} Map {value => count}
  */
 function countOn(x, fn=null, ths=null) {
   var fn = fn||id;
   var m = new Map(), i = -1;
   for(var v of x) {
-    var v1 = fn.call(ths, v, i, x);
-    m.set((m.get(v1)||0) + 1);
+    var v1 = fn.call(ths, v, ++i, x);
+    m.set(v1, (m.get(v1)||0) + 1);
   }
   return m;
 }
