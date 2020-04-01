c99d0a87be58788e36bd5e1f92842a1520bc7b8f
Subhajit Sahu <wolfram77@gmail.com>
2020-04-01 17:57:19 +0530

    Update build.js

--- build.js
+++ build.js
@@ -342,10 +342,10 @@ function setWiki(md, o) {
 function setWikiLinks(md, o) {
   var txt = md.replace(/```.*?```/gs, '');
   var links = new Set();
-  var rref = /(.?)\[([\w\-$.]+)\]/g, m = null;
+  var rref = /(.?)\[([\w\-$.]+)\](.?)/g, m = null;
   var rlink = /^\[([\w\-$.]+)\]\s*:\s*(.*?)$/gm, m = null;
   while((m=rref.exec(txt))!=null)
-    if(m[1]!=='!') links.add(m[2]);
+    if(m[1]!=='!' && m[3]!=='(') links.add(m[2]);
   while((m=rlink.exec(txt))!=null)
     links.delete(m[1]);
   for(var l of links) {
