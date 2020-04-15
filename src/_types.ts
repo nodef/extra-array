46adec33e6dbdbe97258e67d5d3afce4f493f410
wolfram77 <wolfram77@gmail.com>
2020-04-15 21:08:43 +0530

    dont need combineFn anymore

--- src/_types.ts
+++ src/_types.ts
@@ -1,4 +1,3 @@
-export type combineFn = (...as: any[]) => any;
 export type compareFn<T> = (a: T, b: T) => number;
 export type mapFn<T, U> = (v: T, i: number, x: T[]) => T|U;
 export type testFn<T> = (v: T, i: number, x: T[]) => boolean;
