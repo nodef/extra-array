412fa933bf8b94187993cbbb0885881b24c46466
Subhajit Sahu <wolfram77@gmail.com>
2020-05-05 19:14:36 +0530

    indexRange does not need types

--- src/indexRange.ts
+++ src/indexRange.ts
@@ -2,10 +2,10 @@ import index from './index?';
 
 /**
  * Gets index range of part of array.
- * @param {Array} x an array
- * @param {number?} i start index (-ve: from right) (0)
- * @param {number?} I end index (-ve: from right) (end)
- * @returns {number} [start index, end index]
+ * @param x an array
+ * @param i start index (-ve: from right) (0)
+ * @param I end index (-ve: from right) (end)
+ * @returns [start index, end index]
  */
 function indexRange<T>(x: T[], i: number=0, I: number=x.length): [number, number] {
   i = index(x, i);
