5fb5728ee3314ddc17885d83f102e479c27ed65f
Subhajit Sahu <wolfram77@gmail.com>
2020-05-07 22:59:56 +0530

    cycle add n -ve condition check

--- src/cycle.ts
+++ src/cycle.ts
@@ -1,13 +1,13 @@
 import concat$ from './concat$';
 
 /**
- * Gives values that cycle through an array.
+ * Gives values that cycle through array.
  * @param x an array
  * @param n number of values
  */
 function cycle<T>(x: T[], n: number): T[] {
   var a = [], X = x.length;
-  if(X===0) return a;
+  if(n<=0 || X===0) return a;
   for(var i=0, I=Math.floor(n/X); i<I; i++)
     concat$(a, x);
   return concat$(a, x.slice(0, n % X));
