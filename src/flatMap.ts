bf55519f27f03c3b426fc687ca1dde036558d37a
Subhajit Sahu <wolfram77@gmail.com>
2020-05-11 13:21:32 +0530

    :detective: a flatMap now

--- src/flatMap.ts
+++ src/flatMap.ts
@@ -1,3 +1,21 @@
-function flatMap() {
+import concat$ from './concat$';
+import id from './_id';
+import type {mapFn} from './_types';
+
+/**
+ * Flattens nested array, based on map function.
+ * @param x an array
+ * @param fn map function (v, i, x)
+ * @param ths this argument
+ */
+function flatMap(x: Iterable<any>, fn: mapFn<any, any>=null, ths: object=null): any[] {
+  var fn = fn||id;
+  var a = [], i = -1;
+  for(var v of x) {
+    var v1 = fn.call(ths, v, ++i, x);
+    if(Array.isArray(v1)) concat$(a, v1);
+    else a.push(v1);
+  }
+  return a;
 }
 export default flatMap;
