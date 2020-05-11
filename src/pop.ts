7437115d3a3edc0151e659f76c71167209217232
Subhajit Sahu <wolfram77@gmail.com>
2020-05-11 12:59:17 +0530

    :detective: cracks were caused by? pop()

--- src/pop.ts
+++ src/pop.ts
@@ -1,9 +1,8 @@
 /**
  * Removes last value.
  * @param x an array
- * @returns [value, array]
  */
-function pop<T>(x: T[]): [T, T[]] {
-  return [x[x.length-1], x.slice(0, -1)];
+function pop<T>(x: T[]): T[] {
+  return x.slice(0, -1);
 }
 export default pop;
