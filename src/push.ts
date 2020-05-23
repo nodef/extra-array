f4acf6798d5aad19ee7467e676e6d1e4a0b8a93f
Subhajit Sahu <wolfram77@gmail.com>
2020-05-23 14:20:56 +0530

    :peanuts: park.enter(animal) save

--- src/push.ts
+++ src/push.ts
@@ -2,7 +2,6 @@
  * Adds values to the end. 
  * @param x an array
  * @param vs values to add
- * @returns array
  */
 function push<T>(x: T[], ...vs: T[]): T[] {
   return x.concat(vs);
--- src/unshift.ts
+++ src/unshift.ts
@@ -4,7 +4,6 @@ import concat$ from './concat$';
  * Adds values to the start.
  * @param x an array
  * @param vs values to add
- * @returns array
  */
 function unshift<T>(x: Iterable<T>, ...vs: T[]): T[] {
   return concat$(vs, x);
