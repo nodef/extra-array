86ff0af311cddd5449143366604b35e71631cafd
Subhajit Sahu <wolfram77@gmail.com>
2020-06-06 15:39:34 +0530

    :watermelon: cool fruit v2.8.42

--- src/_types.ts
+++ src/_types.ts
@@ -1,3 +1,6 @@
+export type Entries<T>      = Iterable<[number, T]>;
+export type Lists<T>        = [Iterable<number>, Iterable<T>];
+
 export type tillFn          = (dones: boolean[]) => boolean;
 export type reduceFn<T, U>  = (acc: U, v: T, i: number, x: Iterable<T>) => U;
 export type calledFn<T>     = (v: T, i: number, x: Iterable<T>) => void;
@@ -6,5 +9,3 @@ export type mapFn<T, U>     = (v: T, i: number, x: Iterable<T>) => U;
 export type combineFn<T>    = (a: T, b: T) => T;
 export type compareFn<T>    = (a: T, b: T) => number;
 export type getFn<T>        = () => T;
-
-
