41187049ec3fe7569d1cdae17008635800ca3f87
Subhajit Sahu <wolfram77@gmail.com>
2020-06-06 18:42:08 +0530

    :watermelon: cool fruit ongoing maths problem

--- src/infix.ts
+++ src/infix.ts
@@ -1,5 +1,23 @@
 import random from './_random';
 
+function infixLength(X, r) {
+  var C = 0.5*X*(X+1) +1;
+  var c = r*C;
+  var n = 0.5*Math.sqrt(1+ 8*r*C) -0.5;
+  var x = X+1-Math.floor(n+1)
+  console.log(c.toFixed(2), ':', n.toFixed(2), ':', x);
+  return X+1-Math.floor(n+1);
+}
+
+function infixLength(X, r) {
+  var C = 0.5*X*(X+1) + 1;
+  var c = Math.floor(r*C);
+  var n = Math.sqrt(1+ 8*c)*0.5 - 0.5;
+  var x = n===0? 0 : X+1-Math.ceil(n);
+  console.log(c.toFixed(2), ':', n.toFixed(2), ':', x);
+  return X+1-Math.ceil(n);
+}
+
 /**
  * Picks an arbitrary infix.
  * @param x an array
@@ -8,7 +26,7 @@ import random from './_random';
  */
 function infix<T>(x: T[], n: number=-1, r: number=Math.random()): T[] {
   var X = x.length, rnd = random(r);
-  var n = n>=0? n:Math.floor((X+1)*rnd());
+  var n = n>=0? n : infixLength(X, rnd());
   var i = Math.floor((X+1-n)*rnd());
   return n>X? null:x.slice(i, i+n);
 }
