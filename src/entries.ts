328606a567d23cafe6619e590fe65d2d442e2030
Subhajit Sahu <wolfram77@gmail.com>
2020-06-06 15:42:44 +0530

    :watermelon: cool fruit v2.8.43

--- src/entries.ts
+++ src/entries.ts
@@ -1,10 +1,11 @@
 import iterableEntries from '@extra-iterable/entries';
+import type {Entries} from './_types';
 
 /**
  * Lists all index-value pairs.
  * @param x an array
  */
-function* entries<T>(x: Iterable<T>): IterableIterator<[number, T]> {
+function* entries<T>(x: Iterable<T>): Entries<T> {
   yield* iterableEntries(x);
 }
 export default entries;
