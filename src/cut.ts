07e79441952ce823fcfb1b4d2dd9e58640829375
Subhajit Sahu <wolfram77@gmail.com>
2020-06-16 15:00:47 +0530

    :watermelon: cool fruit ;cut

--- src/cut.ts
+++ src/cut.ts
@@ -1,18 +1,18 @@
-import index from './index!';
+import type {testFn} from './_types';
 
 /**
- * Breaks array at given indices.
+ * Breaks array when test passes.
  * @param x an array
- * @param is split indices (sorted)
+ * @param fn test function (v, i, x)
  */
-function cut<T>(x: T[], is: Iterable<number>): T[][] {
+function cut<T>(x: T[], fn: testFn<T>): T[][] {
   var a = [], j = 0;
-  for(var i of is) {
-    i = Math.max(j, index(x, i));
+  for(var i=0, I=x.length; i<I; i++) {
+    if(!fn(x[i], i, x)) continue;
     a.push(x.slice(j, i));
     j = i;
   }
-  a.push(x.slice(i));
+  a.push(x.slice(j));
   return a;
 }
 export default cut;
--- src/cutRight.ts
+++ src/cutRight.ts
@@ -1,12 +1,18 @@
-import {map} from 'extra-iterable';
-import cut from './cut';
+import type {testFn} from './_types';
 
 /**
- * Breaks array after given indices.
+ * Breaks array after test passes.
  * @param x an array
- * @param is split indices (sorted)
+ * @param fn test function (v, i, x)
  */
-function cutRight<T>(x: T[], is: Iterable<number>): T[][] {
-  return cut(x, map(is, i => i+1));
+function cutRight<T>(x: T[], fn: testFn<T>): T[][] {
+  var a = [], j = 0;
+  for(var i=0, I=x.length; i<I; i++) {
+    if(!fn(x[i], i, x)) continue;
+    a.push(x.slice(j, i+1));
+    j = i+1;
+  }
+  a.push(x.slice(j));
+  return a;
 }
 export default cutRight;
