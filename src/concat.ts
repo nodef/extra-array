e0cb3f073dbd295993098216316d931ea35f43db
Subhajit Sahu <wolfram77@gmail.com>
2020-06-19 22:39:59 +0530

    :coconut: chutney ;concat

--- src/concat.ts
+++ src/concat.ts
@@ -1,10 +1,8 @@
-import concat$ from './concat$';
-
 /**
  * Appends values from arrays.
  * @param xs arrays
  */
 function concat<T>(...xs: Iterable<T>[]): T[] {
-  return concat$([], ...xs);
+  return [].concat(...xs);
 }
 export default concat;
