315e080ba8285f5e3766f10d166456bbee305107
Subhajit Sahu <wolfram77@gmail.com>
2020-06-20 19:31:36 +0530

    :coconut: chutney ;unique

--- src/unique.ts
+++ src/unique.ts
@@ -2,25 +2,28 @@ import id from './_id';
 import cmp from './_cmp';
 import type {compareFn, mapFn} from './_types';
 
-function* uniqueMap<T, U=T>(x: Iterable<T>, fn: mapFn<T, T|U>=null): IterableIterator<T> {
+function uniqueMap<T, U=T>(x: Iterable<T>, fn: mapFn<T, T|U>=null): T[] {
   var fn = fn||id;
-  var us = new Set(), i = -1;
+  var s = new Set();
+  var a = [], i = -1;
   for(var v of x) {
     var v1 = fn(v, ++i, x);
-    if(us.has(v1)) continue;
-    us.add(v1); yield v;
+    if(s.has(v1)) continue;
+    s.add(v1); a.push(v);
   }
+  return a;
 }
 
-function* uniqueDual<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): IterableIterator<T> {
+function uniqueDual<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): T[] {
   var fc=fc||cmp, fm = fm||id;
-  var us = [], i = -1;
+  var s = [], a = [], i = -1;
   x: for(var v of x) {
     var v1 = fm(v, ++i, x);
-    for(var u1 of us)
+    for(var u1 of s)
       if(fc(u1, v1)===0) continue x;
-    us.push(v1); yield v;
+    s.push(v1); a.push(v);
   }
+  return a;
 }
 
 /**
@@ -29,8 +32,8 @@ function* uniqueDual<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<
  * @param fc compare function (a, b)
  * @param fm map function (v, i, x)
  */
-function* unique<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): IterableIterator<T> {
-  if(fc) yield* uniqueDual(x, fc, fm);
-  else yield* uniqueMap(x, fm);
+function unique<T, U=T>(x: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): T[] {
+  if(fc) return uniqueDual(x, fc, fm);
+  else return uniqueMap(x, fm);
 }
 export default unique;
