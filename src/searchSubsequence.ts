f2dc2cde43b63daa3672329d387f1013cd485ea5
Subhajit Sahu <wolfram77@gmail.com>
2020-06-24 07:55:36 +0530

    :coconut: chutney more generic searchSubsequence

--- src/searchSubsequence.ts
+++ src/searchSubsequence.ts
@@ -9,16 +9,15 @@ import type {compareFn, mapFn} from './_types';
  * @param fc compare function (a, b)
  * @param fm map function (v, i, x)
  */
-function searchSubsequence<T, U=T>(x: Iterable<T>, y: T[], fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): number {
+function searchSubsequence<T, U=T>(x: Iterable<T>, y: Iterable<T>, fc: compareFn<T|U>=null, fm: mapFn<T, T|U>=null): number {
   var fc = fc||cmp, fm = fm||id
-  var J = y.length, a = -1;
-  var i = -1, j = 0;
-  var y1 = y.map(fm);
+  var y1 = [...y].map(fm), Y = y1.length;
+  var a = -1, i = -1, j = 0;
   for(var u of x) {
     var u1 = fm(u, ++i, x);
     if(fc(u1, y1[j])!==0) continue;
     if(a<0) a = i;
-    if(++j>=J) return a;
+    if(++j>=Y) return a;
   }
   return -1;
 }
