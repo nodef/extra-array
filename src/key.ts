102c8e1cbacdf61b0b3641719425dbf14b774662
Subhajit Sahu <wolfram77@gmail.com>
2020-06-28 18:26:45 +0530

    :coconut: chutney wiki ;has*

--- src/key.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Picks an arbitrary key.
- * @param x an array
- * @param r random seed 0->1
- */
-function key<T>(x: T[], r: number=Math.random()): number {
-  return Math.floor(r * x.length);
-}
-export default key;
