139ff48886886decf733367de318e11a3f787eec
Subhajit Sahu <wolfram77@gmail.com>
2022-02-19 13:54:27 +0530

    :turtle: fix security vulnerabilities

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default [{
-  input: "src/index.js",
+  input: ".build/index.js",
   output: {
     file: "index.js",
     format: "cjs",
@@ -10,7 +10,7 @@ export default [{
   },
   plugins: [resolve(), commonjs()]
 }, {
-  input: "src/index.js",
+  input: ".build/index.js",
   output: {
     file: "index.mjs",
     format: "es",
